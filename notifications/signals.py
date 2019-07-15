45d7d0c6199e42582e2161575aa03b4b392d9ac3
alexHooper <alexmohack@gmail.com>
2019-07-16 00:43:38 +0530

    remove Notification import

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,24 +1,8 @@
 ''' Django notifications signal file '''
 # -*- coding: utf-8 -*-
-from django.dispatch import Signal, receiver
-from django.db.models.signals import post_save
-from django.core.mail import EmailMessage
-
-from .models import Notification
+from django.dispatch import Signal
 
 notify = Signal(providing_args=[  # pylint: disable=invalid-name
 	'recipient', 'actor', 'verb', 'action_object', 'target', 'description',
 	'timestamp', 'level'
 ])
-
-@receiver(post_save, sender=Notification)
-def send_email_notification(sender, instance, created, *args, **kwargs):
-	email = EmailMessage(
-		instance.verb,
-		f'{instance.description} happened {instance.timesince} ago',
-		'New Notification <notifications@example.com>',
-		[instance.recipient.email],
-		reply_to='Support <support@example.com'
-	)
-	email.content_subtype = 'html'
-	email.send()
